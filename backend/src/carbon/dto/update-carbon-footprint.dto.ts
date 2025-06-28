85790abfba0012a65729c217c97fb04839d81ae8
Anthony-Ikpe Godspower <111214429+ObiomaIkpe@users.noreply.github.com>
2025-06-28 18:44:34 +0100

    Fix backend entry point configuration

--- backend/src/carbon/dto/update-carbon-footprint.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateCarbonFootprintDto } from './create-carbon-footprint.dto';
-
-export class UpdateCarbonFootprintDto extends PartialType(CreateCarbonFootprintDto) {}
--- backend/src/marketplace/dto/update-carbon-credit.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateCarbonCreditDto } from './create-carbon-credit.dto';
-
-export class UpdateCarbonCreditDto extends PartialType(CreateCarbonCreditDto) {}
